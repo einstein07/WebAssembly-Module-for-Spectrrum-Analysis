5c1f7f336e9276895a4f803704631905df87823e
einstein07 <mkhsin035@myuct.ac.za>
2022-02-10 22:56:20 +0200

    Added timing for benchmarking.

--- matlab code/test.m
+++ matlab code/test.m
@@ -1,7 +1,7 @@
 %GETFREQREP Computes the fft of a given signal.
 %   Detailed explanation goes here
 dataFileName = 'data.bin';
-L = 1000;
+L = 2048;%1000;
 Fs = 3125000;
 % Load test data 
 fileID = fopen(dataFileName);
@@ -12,11 +12,16 @@ real_part = iq(1:2:end);
 % Next, calculate the frequency axis, which is defined by the sampling rate
 F_axis = (Fs/2)*linspace(0,1,L);
 
-[Y, Y_SHIFTED] = getFreqRep(real_part);
-display (Y_SHIFTED);
+%[Y, Y_SHIFTED] = getFreqRep(real_part);
+%display (Y_SHIFTED);
+f = @()getFreqRep(real_part);
+t = timeit(f);
+display(t);
+Y = getFreqRep(real_part);
 % Plot the signal
+%display(Y);
 figure(2);
-plot(F_axis, abs(Y_SHIFTED));
+plot(F_axis, Y);
 title('Magnitude FFT of signal');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
